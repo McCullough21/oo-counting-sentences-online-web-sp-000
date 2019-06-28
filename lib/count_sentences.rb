8bc55781d48fd99fc3edac4ce0df237237c1e3aa
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-28 20:15:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,7 +18,7 @@ require 'pry'
 delimiters = [". ", "! ", "? "]
   def count_sentences
     delimiters = [". ", "! ", "? "]
-    self.split([". ", "! ", "? "]).count
+    self.split(Regexp.union(delimiters)).count
   end
 # binding.pry
 end
